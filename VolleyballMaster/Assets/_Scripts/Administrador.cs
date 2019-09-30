e710a20a9d89b192d978ee6dc4e2129f3c4758f9
Alejandro <og2189@gmail.com>
2019-09-29 23:25:00 -0500

    Commit de prueba

---
+++ VolleyballMaster/Assets/_Scripts/Administrador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Administrador : MonoBehaviour
+{
+    private string nombre { get; set; }
+    private long identificacion { get; set; }
+    private List<Cancha> canchas;
+    private List<Partido> partidos;
+    private List
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VolleyballMaster/Assets/_Scripts/Cancha.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cancha : MonoBehaviour
+{
+    private string nombre { get; set; }
+    private bool disponible { get;set }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
