1089c12a0f74d4b47bb79eea723390c0cdd2eb6e
saralunaangel <saangela@unal.edu.co>
2020-02-09 20:51:41 -0500

    Created Sign up manager.

---
+++ VolleyballMaster/Assets/_Scripts/SignUpManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUpManager : MonoBehaviour
+{
+
+    public InputField aUsername;
+    public InputField pUsername;
+    public InputField cUsername;
+    public InputField aPassword;
+    public InputField pPassword;
+    public InputField cPassword;
+    public InputField aTeamName;
+
+
+    public void RegistrationA()
+    {
+
+
+
+    }
+
+
+}
